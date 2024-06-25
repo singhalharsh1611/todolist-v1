c5f36dc2e4e88c7596c23b3e33d54ed13b1cd4f7
Harsh Singhal <singhalharsh1611@gmail.com>
2024-06-25 15:10:06 +0530

    added alert for empty task

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 var items = [];
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var d = new Date();
@@ -17,17 +17,26 @@ app.get("/", function(req, res){
     var monthName = months[d.getMonth()];
     var year = d.getFullYear();
     var formattedDate = `${dayName}, ${date} ${monthName} ${year}`;
-    res.render("list", {currDay:formattedDate, newListItem:items});
-    
+    res.render("list", { currDay: formattedDate, newListItem: items });
+
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     var item = req.body.newItem;
+
     console.log(item);
-    items.push(item);
-    res.redirect("/"); 
+
+    if (item == '') {
+        res.send('<script>alert("Please write something"); window.location="/";</script>');
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+
 
 });
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server started on port 3000");
 })
